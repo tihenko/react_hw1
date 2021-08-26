77b9beebc84567d6d5cbc98871d818c4e175c161
Alindos <tihenko2906@gmail.com>
2021-08-26 11:58:13 +0300

    empty

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import {
-  BrowserRouter as Router, Switch, Route, Link, withRouter
-} from "react-router-dom";
 import './App.css';
-
-import { Cars } from "./components/cars/Cars";
-import { Form } from "./components/form/Form";
-
 
 export default function App() {
   return (
+      <div>
 
-      <Router>
-        <div>
-          <Link to={'/'}>Home</Link>
-          <hr/>
-          <Link to={'/cars'}>Cars</Link>
-          <br/>
-          <hr/>
-          <Link to={'/create-car'}>Create car</Link>
-        </div>
-
-        <div>
-          <Route path={'/cars'} component={Cars}/>
-          <Route path={'/create-car'} component={Form}/>
-        </div>
-      </Router>
+      </div>
   );
 }
 
