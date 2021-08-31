6a4b79128a4eb978faf8b87f0e09e998df7b0cc8
Alindos <tihenko2906@gmail.com>
2021-08-31 20:44:59 +0300

    hw7

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { useReducer } from "react";
+
+const reducer = (state =0, action) => {
+  switch (action) {
+    case '+':
+      return state + 10;
+    case '-':
+      return state - 2;
+    default:
+      return state
+  }
+
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer);
+
   return (
       <div>
-
+        <h1>{state}</h1>
+        <button onClick={() => dispatch('+')}>dispatch</button>
+        <button onClick={() => dispatch('-')}>dispatch</button>
       </div>
   );
 }
