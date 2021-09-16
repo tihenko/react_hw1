903a9b0b8dfe66504a6545eeffb9810dedeba942
Alindos <tihenko2906@gmail.com>
2021-09-16 12:33:32 +0300

    redux new part1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+// import logo from './logo.svg';
 
-export default function App() {
+const SomeNestedChildComponent = () => {
+  const counter = useSelector(({ counterValue }) => counterValue);
+  const posts = useSelector(({ posts }) => posts);
+  console.log(counter, 'asdgfd')
+
   return (
-      <div>
+      <header className="App-header">
+        <h1>{counter}</h1>
+        {
+          posts.map(post => (
+              <div key={post.id}>
+                <p>{post.title}</p>
+                <p>{post.body}</p>
+              </div>
+          ))
+        }
+      </header>
 
+  )
+}
+const SomeChildComponent = () => {
+  return (
+      <SomeNestedChildComponent/>
+  )
+}
+
+function App() {
+  const dispatch = useDispatch();
+
+  const fetchPosts = async () => {
+    // const data = await (await fetch('https://jsonplaceholder.typicode.com/posts')).json();
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+
+    dispatch({
+      type: 'SET_POSTS',
+      payload: data,
+    })
+  }
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+  return (
+      <div className="App">
+        <button onClick={() => {
+          dispatch({ type: 'INC' })
+        }}>inc
+        </button>
+        <SomeChildComponent/>
       </div>
   );
 }
 
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = {
+  counterValue: 0,
+  posts: []
+}
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INC': {
+      return { ...state, counterValue: state.counterValue + 1 };
+    }
+    case 'DEC': {
+      return { ...state, counterValue: state.counterValue - 1 };
+    }
+    case 'RESET': {
+      return { ...state, counterValue: 0 };
+    }
+    case 'SET_POSTS': {
+      return { ...state, posts: action.payload};
+    }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counterReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
