748e4bcc3755e7f20ecd517dd588db4b49dd8562
Alindos <tihenko2906@gmail.com>
2022-01-18 10:12:15 +0200

    empty

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
 import './App.css';
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 
-import { getUsers, saveUser } from "./services/api.service";
-import { loadUsers } from "./redux/actionCreaters";
-
-
-
-
-
-
-export default function App() {
-
-  let state = useSelector(state => state)
-  let dispatch = useDispatch();
-  let { users } = state
-
-  useEffect(() => {
-    getUsers().then(value => {
-      dispatch(loadUsers(value));
-    });
-  }, []);
-
-  let handleAddUsers = async (e) => {
-    let user = { name: 'vasya' }
-    await saveUser(dispatch, user);
-
-
-  };
-
+export const App = () => {
   return (
       <div>
-         <button onClick={handleAddUsers}>add user</button>
-        {
-          users.map(value => <div>{value.name}</div>)
-        }
+
       </div>
-  )
-
+  );
 }
-
-
--- src/index.js
+++ src/index.js
@@ -1,40 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
+
 import './index.css';
-import App from './App';
-
-import {Provider} from 'react-redux' ;
-import {createStore} from "redux";
-import ReactDom from "react-dom";
-import { ADD_USER, LOAD_USERS } from "./redux/actions";
-// import { addUser, loadUsers } from "./redux/actionCreaters";
-
-
-let initialState = {};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case LOAD_USERS :
-      return {...state, users : [...action.payload]}
-    case ADD_USER:
-      let newUser = action.payload;
-      let newUsersArray = [...state.users, newUser];
-      return {...state, users: [...newUsersArray]}
-    default :
-      return state;
-  }
-
-
-}
-
-
-let store = createStore(reducer)
+import { App } from './App';
 
 ReactDom.render(
     <React.StrictMode>
-      <Provider store={store}>
-        <App/>
-      </Provider>
+      <App/>
     </React.StrictMode>,
-document.getElementById('root')
+    document.getElementById('root')
 )
